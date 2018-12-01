5d690b64e7cc44743fc1c295dae13bc94ea1495e
Pawel Warzecha <ProrokWielki@o2.pl>
2018-12-01 19:41:01 +0100

    the setup.py file was added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py_buildsystem',
+      version='0.1',
+      description='pythin based build system.',
+      url='https://github.com/ProrokWielki/py_buildsystem',
+      author='Paweł Warzecha',
+      author_email='pawel.warzecha@yahoo.com',
+      license='MIT',
+      packages=['py_buildsystem'],
+      zip_safe=False)
