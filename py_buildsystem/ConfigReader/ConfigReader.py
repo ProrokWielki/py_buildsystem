772eec902f24e54a4fcf9cd49cec449a8bab2bfa
Pawel Warzecha <ProrokWielki@o2.pl>
2019-01-12 18:20:33 +0100

    ConfigReader

--- py_buildsystem/ConfigReader/ConfigReader.py
+++ py_buildsystem/ConfigReader/ConfigReader.py
@@ -1,20 +1,21 @@
-import json
+import yaml
 
 from abc import ABC, abstractmethod
 
+
 class ConfigReader(ABC):
-    def __init__(self, config_json_file):      
-        self.read_config_file(config_json_file)
-        
+    def __init__(self, config_yaml_file):
+        self.read_config_file(config_yaml_file)
+
         self._check_config()
-        
-    def read_config_file(self, config_json_file):
+
+    def read_config_file(self, config_yaml_file):
         try:
-            with open(config_json_file) as config_file:
-                self.configuration = json.load(config_file)
-        except:
+            with open(config_yaml_file, 'r') as config_file:
+                self.configuration = yaml.load(config_file)
+        except FileNotFoundError:
             raise Exception('Given configuration file does not exist.')
-        
-        @abstractmethod
-        def _check_config(self):
-            pass        
+
+    @abstractmethod
+    def _check_config(self):
+        pass  # pragma: no cover
