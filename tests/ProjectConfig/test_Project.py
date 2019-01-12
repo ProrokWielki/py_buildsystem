c9abd86fd17aee8b319f2d4caae770720b61ba8c
Pawel Warzecha <ProrokWielki@o2.pl>
2019-01-12 18:18:19 +0100

    added unit tests

--- tests/ProjectConfig/test_Project.py
+++ tests/ProjectConfig/test_Project.py
@@ -1,74 +1,93 @@
 import os
-import json
+import yaml
 import shutil
 
 import unittest
+from unittest.mock import Mock
 
-from py_buildsystem.ProjectConfig.ProjectConfig import ProjectConfig
+from py_buildsystem.Project.Project import Project
 
 script_file_path = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
 
 test_files_dir_name = "test_files"
-test_config_file_name = "test_file.json"
-corrupted_test_config_file_name = "corrupted_test_file.json"
+
+test_config_file_name = "test_project.yaml"
+minimal_test_config_file_name = "minimal_test_project.yaml"
 
 test_files_path = os.path.join(script_file_path, test_files_dir_name).replace("\\", "/")
 
 test_config_file_with_path = os.path.join(test_files_path, test_config_file_name).replace("\\", "/")
-corrupted_test_config_file_with_path = os.path.join(test_files_path, corrupted_test_config_file_name).replace("\\", "/")
+test_config_file_no_includes_no_defines_with_path = os.path.join(test_files_path, minimal_test_config_file_name).replace("\\", "/")
+
 
 project_test_configuration = {
-                              "defines": ["STM32L452xx"],
+    "defines": ["DEBUG"],
 
-                              "flags": ["-g", 
-                                        "-T ../../MCU/STM32L452RETx_FLASH.ld", 
-                                        "-mcpu=cortex-m4", 
-                                        "--specs=nosys.specs" ],
-    
-                              "includes": ["../../LIB/CMSIS/Device/ST/STM32L4xx/Include",
-                                           "../../LIB/CMSIS/Include" ],
+    "includes": [
+        "test/inc",
+        "test1/inc"
+    ],
 
-                              "project_name": "Wooden_Clock",
-  
-                              "sources_root_directory": "../../woodenClock/Software",
-                              "output_directory": "../../Output" 
-                              }
+    "steps": [
+        {
+            "compile First": None,
+            "source_directories": ["../../MCU"],
+            "output_direcotry": "../../Output/Obj/MCU",
+            "types": [".c", ".s"]
+        },
+        {
+            "link Second": None,
+            "source_directories": ["../../Output/Obj"],
+            "output_file": "../../Output/file.elf",
+            "types": [".o"]
+        }
+    ]
+}
 
-class TestProjectConfig(unittest.TestCase):        
+empty_project_config = {}
+
+
+class TestProjectConfig(unittest.TestCase):
     @classmethod
-    def setUpClass(cls):      
+    def setUpClass(cls):
         os.makedirs(test_files_path, exist_ok=True)
-        
+
         with open(test_config_file_with_path, "w") as test_file:
-            json.dump(project_test_configuration, test_file, indent=2)
-            
-        with open(corrupted_test_config_file_with_path, "w") as corrupted_test_file:
-            json.dump(dict(list(project_test_configuration.items())[2:]), corrupted_test_file, indent=2) #corrpting the json file, by taking only part of the dictionary.
-                    
-                    
+            yaml.dump(project_test_configuration, test_file, indent=2)
+
+        with open(test_config_file_no_includes_no_defines_with_path, "w") as test_file:
+            yaml.dump(empty_project_config, test_file, indent=2)
+
+        cls.test_project_name = test_config_file_name.split(".")[0]
+        cls.minimal_project_name = minimal_test_config_file_name.split(".")[0]
+
+        cls.toolchain = Mock()
+
     @classmethod
     def tearDownClass(cls):
-        try:
-            shutil.rmtree(test_files_path)
-        except:
-            pass  
-    
+        shutil.rmtree(test_files_path, ignore_errors=True)
+
     def test_read_config_file(self):
-        
-        with self.assertRaises(Exception):
-            ProjectConfig("random_text")
-            
-        with self.assertRaises(Exception):
-            ProjectConfig(corrupted_test_config_file_with_path)
-            
-        project_config = ProjectConfig(test_config_file_with_path)    
+        Project(test_config_file_with_path, self.toolchain)
+        Project(test_config_file_no_includes_no_defines_with_path, self.toolchain)
 
-        self.assertEqual(project_config.get_project_name(), project_test_configuration["project_name"])
-        self.assertEqual(project_config.get_sources_root_directory(), project_test_configuration["sources_root_directory"])
-        self.assertEqual(project_config.get_output_directory(), project_test_configuration["output_directory"])
-    
-    def test_compile(self):
-        pass
+    def test_get_project_name(self):
+        project = Project(test_config_file_with_path, self.toolchain)
+        empty_project = Project(test_config_file_no_includes_no_defines_with_path, self.toolchain)
 
-        
-        
+        self.assertEqual(project.get_project_name(), self.test_project_name)
+        self.assertEqual(empty_project.get_project_name(), self.minimal_project_name)
+
+    def test_get_defines(self):
+        project = Project(test_config_file_with_path, self.toolchain)
+        empty_project = Project(test_config_file_no_includes_no_defines_with_path, self.toolchain)
+
+        self.assertEqual(project.get_defines(), project_test_configuration["defines"])
+        self.assertEqual(empty_project.get_defines(), [])
+
+    def test_get_includes(self):
+        project = Project(test_config_file_with_path, self.toolchain)
+        empty_project = Project(test_config_file_no_includes_no_defines_with_path, self.toolchain)
+
+        self.assertEqual(project.get_includes(), project_test_configuration["includes"])
+        self.assertEqual(empty_project.get_includes(), [])
