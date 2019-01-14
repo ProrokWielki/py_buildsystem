d381441400805cd1ddf88ece9bdf3c1351be6e5e
Pawel Warzecha <ProrokWielki@o2.pl>
2019-01-14 20:35:14 +0100

    fixed verbose mode

--- py_buildsystem/__main__.py
+++ py_buildsystem/__main__.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse
 
@@ -12,21 +13,21 @@ sys.tracebacklimit = 0
 
 parser = argparse.ArgumentParser(prog="py_buildsystem", description='Python based build system.', allow_abbrev=True)
 
-parser.add_argument('project_compiler_config', metavar='-pcc', type=str, nargs=1,
+parser.add_argument('-pcc', '--project_compiler_config', type=str, nargs=1, required=True,
                     help='Project specific toolchain configuration file')
 
-parser.add_argument('project_config', metavar='-pc', type=str, nargs=1,
+parser.add_argument('-pc', '--project_config', type=str, nargs=1, default=os.getcwd(), required=True,
                     help='Project configuration file')
 
 parser.add_argument('compiler_path', metavar='compiler path', type=str, nargs='?', default='',
                     help='Path to compiler')
 
-parser.add_argument('-v', '--verbose', action='store_true',
+parser.add_argument('-v', '--verbose', action='store_true', dest="verbose",
                     help='verbose mode')
 
 args = parser.parse_args()
 
-if args.v is True:
+if args.verbose is True:
     logger.setLevel(levels["DEBUG"])
 else:
     logger.setLevel(levels["INFO"])
